b952fec56b63db8f0827a4cfbcd7f15b42a6000d
alexp <alexamthi67mail.com>
2022-03-31 20:00:09 -0300

    se agrega el proyecto

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+let btnMenu = document.getElementById('btn-menu');
+let mainNav = document.getElementById('main-nav');
+btnMenu.addEventListener('click', function(){
+  mainNav.classList.toggle('mostrar');
+});
+
